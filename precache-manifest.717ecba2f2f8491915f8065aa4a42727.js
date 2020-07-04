f8cb23447b6118a6ef99365ed5dff187aaebb9d0
Christian <christianphalv@gmail.com>
2020-07-03 23:04:00 -0500

    Updates

---
+++ precache-manifest.717ecba2f2f8491915f8065aa4a42727.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f62a35de5faf01254ae44fbbcccf270",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4ca9331432ab0489eb41",
+    "url": "/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "8256c916e3eac91d9735",
+    "url": "/static/css/main.3a326062.chunk.css"
+  },
+  {
+    "revision": "4ca9331432ab0489eb41",
+    "url": "/static/js/2.5b31d188.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.5b31d188.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8256c916e3eac91d9735",
+    "url": "/static/js/main.2f844099.chunk.js"
+  },
+  {
+    "revision": "c1a021fd683433281187",
+    "url": "/static/js/runtime-main.3c542571.js"
+  }
+]);
