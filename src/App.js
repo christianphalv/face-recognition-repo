de278e0e840a8a1ce7f66925c69202604a09e272
Christian <christianphalv@gmail.com>
2020-07-03 21:50:54 -0500

    first changes to view

--- src/App.js
+++ src/App.js
@@ -1,12 +1,39 @@
 import React from 'react';
+import Particles from 'react-particles-js';
+import Navigation from './components/navigation/Navigation';
+import Logo from './components/logo/Logo';
+import ImageLinkForm from './components/imageLinkForm/ImageLinkForm';
+import Rank from './components/rank/Rank.js';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
+const particlesOptions = {
+	particles: {
+		number: {
+			value: 100,
+			density: {
+				enable: true,
+				value_area: 800
+			}
+		}
+	}
+}
 
-    </div>
-  );
+class App extends React.Component {
+	render() {
+		return (
+			<div className="App">
+				<Particles className="particles"
+                params={particlesOptions} />
+				<Navigation />
+				<Logo />
+				<Rank />
+				<ImageLinkForm />
+				{/*
+				<ImageLinkForm />
+				<FaceRecognition />*/}
+			</div>
+		  );
+	}
 }
 
 export default App;
